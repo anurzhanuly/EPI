5cfe90020dfc517656ad6d2879da0a2f2f06700a
anurzhanuly <isaxved@gmail.com>
2021-09-15 23:17:41 +0600

    brute force solution

--- first_task.py
+++ first_task.py
@@ -1,2 +1,20 @@
 def author_h_index(book_titles: list, book_citations: list)-> int:
-    pass
+    counter: int = 1
+
+    for _ in book_citations:
+        tmp: int = 0
+
+        for number in book_citations:
+            if counter <= number:
+                tmp += 1
+        
+        if tmp < counter:
+            break
+
+        counter += 1
+
+    return counter-1
+
+print(author_h_index([], [1, 2, 3, 4, 5]))
+print(author_h_index([], [4, 4, 4, 4, 5]))
+print(author_h_index([], [1, 2, 2, 1, 0]))
