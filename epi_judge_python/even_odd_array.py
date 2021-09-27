61d1c72242581336cd6a5d4f72cc9e92a2ee2057
nurzhanuly <nurzhanuly@kolesa.kz>
2021-09-28 02:21:45 +0600

    first task of arrays

--- epi_judge_python/even_odd_array.py
+++ epi_judge_python/even_odd_array.py
@@ -6,11 +6,44 @@ from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
-
+# [0, 1, 2, 3, 4, 5, 6, 7, 8]
 def even_odd(A: List[int]) -> None:
-    # TODO - you fill in here.
+    start: int = 0
+    end: int = len(A)-1
+
+    while start < end:
+        if is_even(A[start]):
+            start += 1
+
+            if is_odd(A[end]):
+                end -= 1
+        
+            continue
+
+        if is_even(A[end]):
+            A = swap_values(start, end, A)
+
+            start += 1
+            end -= 1
+        else:
+            end -= 1
+
     return
 
+def is_odd(number: int) -> bool:
+    return number % 2 != 0
+
+def is_even(number: int) -> bool:
+    return number % 2 == 0
+
+def swap_values(a: int, b: int, arr: List[int]) -> List[int]:
+    tmp: int = arr[a]
+    arr[a] = arr[b]
+    arr[b] = tmp
+
+    return arr 
+
+    
 
 @enable_executor_hook
 def even_odd_wrapper(executor, A):
